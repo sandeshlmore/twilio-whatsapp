320c4436c2513d5f8a98097c9ae97d0c127dbd08
Sandesh More <sandeshmore@gofynd.com>
2022-04-23 15:14:15 +0530

    added health-check endpoint

--- wsgi.py
+++ wsgi.py
@@ -2,12 +2,14 @@ from werkzeug import run_simple
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 from apis.messaging.views import message_blueprint
+from apis.health_check import health_blueprint
 from master.app import app
 
 app.register_blueprint(message_blueprint)
+app.register_blueprint(health_blueprint)
 
 
-application = DispatcherMiddleware(None, {
+application = DispatcherMiddleware(app, {
     '/messaging': app
 })
 
